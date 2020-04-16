0a36d30451d88dbca154a1e5fcc828ee1e6feecc
James Rawlings <rawlingsj80@gmail.com>
2020-04-16 13:20:28 +0100

    fix: helmfile missing chart prefixes

--- pkg/cmd/step/create/helmfile/create_helmfile.go
+++ pkg/cmd/step/create/helmfile/create_helmfile.go
@@ -204,6 +204,13 @@ func (o *CreateHelmfileOptions) generateHelmFile(ec *envctx.EnvironmentContext, 
 					matched = true
 				}
 			}
+			// if matched make sure the prefix appears in the chartname
+			if matched {
+				prefix := helmPrefixes.PrefixForURL(details.Repository)
+				if prefix == "" {
+					details.Name = repos[details.Repository] + "/" + details.Name
+				}
+			}
 			if !matched {
 				prefix := helmPrefixes.PrefixForURL(details.Repository)
 				if prefix == "" {
@@ -212,6 +219,7 @@ func (o *CreateHelmfileOptions) generateHelmFile(ec *envctx.EnvironmentContext, 
 				if prefix == "" {
 					prefix = uuid.New().String()
 				}
+				details.Name = prefix + "/" + details.Name
 				repos[details.Repository] = prefix
 			}
 		}
